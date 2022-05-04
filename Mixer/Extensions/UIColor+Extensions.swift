607187daa47a4a915e258afe2af0c7aa4a2f8095
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2022-05-03 23:59:02 -0400

    General UI changes, plist font additions

--- Mixer/Extensions/UIColor+Extensions.swift
+++ Mixer/Extensions/UIColor+Extensions.swift
@@ -5,4 +5,32 @@
 //  Created by Neftali Samarey on 5/3/22.
 //
 
-import Foundation
+import UIKit
+
+public extension UIColor {
+
+    convenience init(hex: String) {
+        var color: UInt32 = 0
+
+        let isAlphaIncluded = hex.count == 8
+        let shiftRed = isAlphaIncluded ? 24 : 16
+        let shiftGreen = isAlphaIncluded ? 16 : 8
+        let shiftBlue = isAlphaIncluded ? 8 : 0
+
+        let scanner = Scanner(string: hex)
+        scanner.scanHexInt32(&color)
+
+        let mask = 0x000000FF
+        let r = Int(color >> shiftRed) & mask
+        let g = Int(color >> shiftGreen) & mask
+        let b = Int(color >> shiftBlue) & mask
+        let a = isAlphaIncluded ? Int(color) & mask : 100
+
+        let red = CGFloat(r) / 255
+        let green = CGFloat(g) / 255
+        let blue = CGFloat(b) / 255
+        let alpha = CGFloat(a) / 100
+
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
